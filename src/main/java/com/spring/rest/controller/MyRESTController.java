7f2293201a9fbe9f845b7b1f86ebc1b72f1348f9
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-10 15:03:30 +0200

    create method getEmployee: -get one Employee by id in URL like * /api/employees/{id}

--- src/main/java/com/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/spring/rest/controller/MyRESTController.java
@@ -4,6 +4,7 @@ import com.spring.rest.entity.Employee;
 import com.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,8 @@ import java.util.List;
  * MyRestController "@RestController" = same controller but for HTTP
  * "@RestController" = "@Controller" = "@Component"
  * "@RequestMapping" = base page
+ * <p>
+ * Don`t forget all Object from DB are returning in JSON form
  */
 @RestController
 @RequestMapping("/api")
@@ -30,8 +33,19 @@ public class MyRESTController {
      *
      * @return List<Employee> = all objects from DB
      */
-    @RequestMapping("/employees")
+    @GetMapping("/employees")
     public List<Employee> showAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    /**
+     * The getEmployee by GET request "/employees/{id}" hive one Object from DB
+     *
+     * @param id from key=value URL "/employees/{id}" which User set by himself
+     * @return Employee by id from DB
+     */
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return employeeService.getEmployee(id);
     }
 }
