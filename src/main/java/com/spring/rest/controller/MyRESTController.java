efc2c0cfe9e2c15dec680ee0d0bb0161d682d310
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-10 14:21:57 +0200

    some Rebuild in pom file !!!

---
+++ src/main/java/com/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,37 @@
+package com.spring.rest.controller;
+
+import com.spring.rest.entity.Employee;
+import com.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * MyRestController "@RestController" = same controller but for HTTP
+ * "@RestController" = "@Controller" = "@Component"
+ * "@RequestMapping" = base page
+ */
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    // link to EmployeeService object
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * showAllEmployees "@GetMapping" same "@RequestMapping" but only for
+     * Http request GET
+     * <p>
+     * And link to "api/employees"
+     *
+     * @return List<Employee> = all objects from DB
+     */
+    @RequestMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
